9265bb667f239a18b70f345bebf86cbcbf11a0de
Idir Ammani <Idir@mbp-de-idir-3.home>
2019-06-13 15:15:50 +0200

    pokemon view

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { FlatList, ActivityIndicator, StyleSheet, Text, View, Button } from 'react-native';
+import {createStackNavigator, createAppContainer, StackActions, NavigationActions} from 'react-navigation';
 
 class HomeScreen extends Component {
 
-    render() {
-        return (
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Hello, !</Text>
+    constructor(props){
+        super(props);
+        this.state ={ isLoading: true}
+      }
+      componentDidMount(){
+        return fetch('https://pokeapi.co/api/v2/pokemon/')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            this.setState({
+              isLoading: false,
+              dataSource: responseJson.results,
+            }, function(){
+    
+            });
+    
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
+    
+      render(){
+        if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator size="large" color="#0000ff"/>
+            </View>
+          )
+        }
+    
+        return(
+          <View style={{flex: 1, paddingTop:40}}>
+            <FlatList style={{borderColor:'black'}}
+              data={this.state.dataSource}
+              renderItem={({item}) => 
+              <Button
+          title={item.name}
+          onPress={ () => {this.props.navigation.navigate('Pokemon', {
+            name: item.name,
+          });
+        }}
+        />}
+              keyExtractor={({id}, index) => id}
+            />
           </View>
         );
       }
--- views/PokemonDetails.js
+++ views/PokemonDetails.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import { FlatList, ActivityIndicator, StyleSheet, Text, View, Image } from 'react-native';
+import {createStackNavigator, createAppContainer, NavigationActions, StackActions} from 'react-navigation';
+
+const styles = StyleSheet.create({
+  text_center: {
+    alignSelf: 'center',
+  },
+  text_center_bottom: {
+    alignSelf:'center',
+    marginBottom: 20,
+  },
+  text_center_top: {
+    alignSelf:'center',
+    marginTop: 30,
+  },
+});
 
 class PokemonDetails extends Component {
 
@@ -9,12 +23,22 @@ class PokemonDetails extends Component {
       this.state ={ isLoading: true}
     }
     componentDidMount(){
-      return fetch('https://pokeapi.co/api/v2/pokemon/')
+      const { navigation } = this.props;
+      const pokemon_name = navigation.getParam('name', 'NO-ID');
+      var newurl = 'https://pokeapi.co/api/v2/pokemon/'+pokemon_name;
+      return fetch(newurl)
         .then((response) => response.json())
         .then((responseJson) => {
+          console.log(responseJson.abilities);
           this.setState({
             isLoading: false,
-            dataSource: responseJson.results,
+            PokemonName: responseJson.name,
+            PokemonImage: responseJson.sprites.front_default,
+            PokemonId:responseJson.id,
+            PokemonAbilities: responseJson.abilities,
+            PokemonMoves: responseJson.moves,
+            PokemonStats: responseJson.stats,
+
           }, function(){
   
           });
@@ -33,16 +57,28 @@ class PokemonDetails extends Component {
           </View>
         )
       }
-  
+
+      let pic = {
+        uri: this.state.PokemonImage
+      };
+        
       return(
-        <View style={{flex: 1, paddingTop:40}}>
-          <FlatList style={{borderColor:'black'}}
-            data={this.state.dataSource}
-            renderItem={({item}) => <Text>{item.name}</Text>}
-            keyExtractor={({id}, index) => id}
-          />
-        </View>
+            <View style={{flex: 1, paddingTop:20}}>
+              <Text style={styles.text_center_bottom}>{this.state.PokemonName} n°{this.state.PokemonId}</Text>
+              <Image source={pic} style={{width: 193, height: 160, alignSelf: 'center'}} />
+
+              <Text style={styles.text_center_bottom}>Stats</Text>
+              <FlatList style={{borderColor:'black'}}
+              data={this.state.PokemonStats}
+              renderItem={({item}) => 
+              <Text style={styles.text_center}>{item.stat.name} : {item.base_stat}</Text>}
+              keyExtractor={({id}, index) => id}
+            />
+              
+            </View>
       );
+        
+      
     }
   
   }
